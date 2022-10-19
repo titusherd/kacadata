714b3904b04ef9c04f5e39106afc3cd88d20043b
Faizal H R <rizfianto@gmail.com>
2022-10-19 14:04:08 +0700

    E3-PBI1 adjustment to the LoFi

--- kacadata/SycnPageView.swift
+++ kacadata/SycnPageView.swift
@@ -1,5 +1,5 @@
 //
-//  SwiftUIView.swift
+//  SyncPageView.swift
 //  kacadata
 //
 //  Created by Faizal Hudya Rizfianto on 18/10/22.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
+struct SyncPageView: View {
     let image = Image(systemName: "questionmark.circle.fill")
     
     var body: some View {
@@ -19,18 +19,31 @@ struct SwiftUIView: View {
                     .frame(width: 358, height: 382, alignment: .center)
                     .padding()
                 
-                (Text("Connecting Data \nWithin Our App ") + Text(image))
-                    .font(.system(size: 38, weight: .bold, design: .rounded))
-                    .multilineTextAlignment(.leading)
-                    .padding(EdgeInsets(top: 0, leading: -38, bottom: 20, trailing: 0))
+                HStack{
+//                    (Text("Connecting Data \nWithin Our App ") + Text(image))
+                    Text("Connecting Data \nWithin Our App ")
+                        .font(.system(size: 36, weight: .bold, design: .rounded))
+                        .multilineTextAlignment(.leading)
+
+                    
+                    NavigationLink {
+                        Text("This is page for Help View")
+                    } label: {
+                        Image(systemName: "questionmark.circle.fill")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 38, height: 38, alignment: .center)
+                    }
+                }
+                .padding(EdgeInsets(top: 0, leading: -17, bottom: 20, trailing: 0))
                 
-                Text("Connect your data and let our app do the rest. \nAlign your work, and recommend your promo in accurate.")
-                    .font(.system(size: 18, weight: .regular, design: .rounded))
+                Text("Connect your data and let our app do the rest. \nAlign your work, and recommend your promo in \naccurate.")
+                    .font(.system(size: 16, weight: .regular, design: .rounded))
                     .multilineTextAlignment(.leading)
-                    .padding(EdgeInsets(top: 0, leading: 35, bottom: 40, trailing: 36))
+                    .padding(EdgeInsets(top: 0, leading: -17, bottom: 32, trailing: 0))
                 
                 NavigationLink(
-                    destination: Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/),
+                    destination: Text("This is page for MOKA Sync"),
                     label: {
                         Text("Connect With MOKA")
                             .frame(width: 358, height: 48, alignment: .center)
@@ -39,9 +52,10 @@ struct SwiftUIView: View {
                             .cornerRadius(8)
                     }
                 )
+                .padding(.bottom, 4.0)
                 
                 NavigationLink(
-                    destination: Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/),
+                    destination: Text("Coming Soon!"),
                     label: {
                         Text("Import your report file (.csv)")
                             .frame(width: 358, height: 48, alignment: .center)
@@ -55,8 +69,8 @@ struct SwiftUIView: View {
     }
 }
 
-struct SwiftUIView_Previews: PreviewProvider {
+struct SyncPageView_Previews: PreviewProvider {
     static var previews: some View {
-        SwiftUIView()
+        SyncPageView()
     }
 }
